2f5424d41195ad7516ee83307eef6126b5acc717
Bruno Araujo <bruno.aoliveira@al.infnet.edu.br>
2022-10-23 21:40:02 -0300

    consertando o problema de não renderizar no primeiro load da pagina as infos das artes

--- src/view/CollectionArt/index.jsx
+++ src/view/CollectionArt/index.jsx
@@ -4,33 +4,19 @@ import { TemaContext } from "../../Contexts/TemaContext";
 import { getObjectByIdDepartmentService } from "../../services/arts.service";
 import { getObjectById } from "../../utils/handleHttpRequest";
 import "./index.css";
+import noImage from "../../assets/noImage.png";
 
 export function CollectionArtComponent() {
 
     const { id } = useParams();
     const { tema } = useContext(TemaContext);
 
-    const [artListsId, setArtListsId] = useState([]);
     const [artLists, setArtLists] = useState([]);
     const [auxArtLists, setAuxArtLists] = useState([]);
     const [maxCount, setMaxCount] = useState(20);
     const [minCount, setMinCount] = useState(0);
 
 
-    const getInfoListCollection = async (id) => {
-
-        const list = await getObjectById(id);
-
-
-        setArtLists((item) => [...item, list])
-        setAuxArtLists(list)
-    }
-
-    const ListCollection = async () => {
-        const listId = await getObjectByIdDepartmentService(id)
-        setArtListsId(listId)
-        // console.log('artListsId', artListsId?.data?.objectIDs)
-    };
 
     function handleSearch(e){
 
@@ -57,40 +43,60 @@ export function CollectionArtComponent() {
 
     }
 
+    
+    const getInfoListCollection = async (id) => {
+
+        let list = await getObjectById(id);
+
+            // if (list.primaryImage !== "") {
+                setArtLists((item) => [...item, list])
+                setAuxArtLists((item) => [...item, list])  
+            // }
+    
+
+
+
+    }
+
+    const ListCollection = async () => {
+        const listId = await getObjectByIdDepartmentService(id)
+
+        if (listId?.data?.objectIDs.length > 0) {
+            listId?.data?.objectIDs.forEach((id, index) => {
+                if (index >= minCount && index < maxCount) {
+                    getInfoListCollection(id)
+                }
+            })
+        }
+
+    };
+
 
     useEffect(() => {
-        ListCollection().then(() => {
-
-            if (artListsId?.data?.objectIDs.length > 0) {
-                artListsId?.data?.objectIDs.forEach((id, index) => {
-                    if (index >= minCount && index < maxCount) {
-                        getInfoListCollection(id)
-                    }
-                })
-            }
-
-        })
+        ListCollection()
     }, [minCount])
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        const scrollObserver = new IntersectionObserver((entry) => {
-            if (entry.some(entries => entries.isIntersecting)) {
-                setMinCount(state => state + 20)
-                setMaxCount(state => state + 20)
-                console.log("min", minCount);
-                console.log("maxCount", maxCount);
-            }
-        })
+    //     const scrollObserver = new IntersectionObserver((entry) => {
+    //         if (entry.some(entries => entries.isIntersecting)) {
+    //             setMinCount(state => state + 20)
+    //             setMaxCount(state => state + 20)
+    //             console.log("min", minCount);
+    //             console.log("maxCount", maxCount);
+    //         }
+    //     })
 
-        scrollObserver.observe(document.querySelector('#sentinel'));
+    //     scrollObserver.observe(document.querySelector('#sentinel'));
 
-        return () => scrollObserver.disconnect();
+    //     return () => scrollObserver.disconnect();
 
-    }, [])
+    // }, [])
 
     return (
         <article id="collection-article-container">
+
+            <h1 style={{ color: tema.corTexto }}>{auxArtLists[0]?.department}</h1>
 
             <section className="dept-input-section">
                 <input type="text" placeholder="Search a department" className="dept-input-search" 
@@ -103,19 +109,19 @@ export function CollectionArtComponent() {
             {auxArtLists.length > 0 &&
                 auxArtLists.map((art, idx) => {
 
-                    if (art.primaryImage !== "") {
+                    
                         return (
                             <div className="collection-div-card" key={idx} style={{
                                 borderColor: tema.corBotao, borderStyle: "solid",
                                 backgroundColor: tema.corFundoTema, color: tema.corTexto
                             }}>
                                 <figure className="collection-figure-card">
-                                    <img className="collection-img-card" src={art.primaryImageSmall} alt={art.objectName} />
+                                    <img className="collection-img-card" src={art.primaryImageSmall !== "" ? art.primaryImageSmall : noImage} alt={art.objectName} />
                                 </figure>
                                 <div className="collection-div-specs">
-                                    <h3 className="collection-title-art">{art.title}</h3>
+                                    <h3 className="collection-title-art">{art.title.length < 45 ? art.title : (art.title.slice(0,45) + "...")}</h3>
                                     <div className="collection-div-artist">
-                                        <p>Medium: {art.medium}</p>
+                                        <p>Medium: {art.medium !== "" ? art.medium : "No medium"}</p>
                                         <p>Artist: {art.artistDisplayName !== "" ? art.artistDisplayName : "Unknown"} </p>
                                     </div>
                                     <button className="collection-button-card" style={{ backgroundColor: tema.corBotao, color: tema.corTexto }}>See more</button>
@@ -124,7 +130,7 @@ export function CollectionArtComponent() {
 
                             </div>
                         )
-                    }
+                    
                 })
             }
             <div className="spinner-border text-success" role="status" id="sentinel">
